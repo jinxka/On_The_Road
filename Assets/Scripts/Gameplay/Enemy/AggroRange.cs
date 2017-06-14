177590f2fdb2beede4a3e7c56dcf1dd3f90142d5
balang_b <benjamin.balanger@epitech.eu>
2017-06-14 11:49:04 +0200

    Monsters have now an aggro range and can be linked

---
+++ Assets/Scripts/Gameplay/Enemy/AggroRange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AggroRange : MonoBehaviour {
+
+    EnemyMovement enemyMovement;
+    public bool LinkAggro = false;
+
+    // Use this for initialization
+    void Start () {
+        enemyMovement = GetComponentInParent<EnemyMovement>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            enemyMovement.SetAggro(true);
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (LinkAggro && enemyMovement.GetAggro() && other.tag == "Enemies")
+        {
+            if (!other.GetComponent<EnemyMovement>().GetAggro())
+                other.GetComponent<EnemyMovement>().SetAggro(true);
+        }
+    }
+}
