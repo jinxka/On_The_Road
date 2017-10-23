a29941a2c7183ed80b03a3f6e2277be3fde986e6
Ram <ramy.chidiac@live.fr>
2017-10-23 23:05:41 +0200

    Added some inventory scripts

---
+++ Assets/Scripts/InGameHUD/Inventory and Skills/InventoryItemOnObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemOnObject : MonoBehaviour {
+
+    public InventoryItem item;
+    public Text stacks;
+
+    public void Start()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InGameHUD/Inventory and Skills/InventoryScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour {
+    public List<InventoryItem> itemList;
+    public InventoryTooltip inventoryTooltip;
+
+    public void activateTooltip()
+    {
+
+    }
+}
