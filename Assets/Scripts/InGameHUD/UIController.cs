764c30f0befc3166c6d97573906f0ae17d0518db
Unknown <remy.macmillan@epitech.eu>
2017-12-13 23:44:31 +0100

    Carte du monde /tooltip

--- Assets/Scripts/InGameHUD/UIController.cs
+++ Assets/Scripts/InGameHUD/UIController.cs
@@ -10,6 +10,7 @@ public class UIController : MonoBehaviour {
 	public GameObject mapPanel;
     public Camera cam;
     public Canvas UiCanvas;
+	public CanvasGroup worldMapCanvas;
     [SerializeField]
     Slider ammoSlider;
     [SerializeField]
@@ -21,21 +22,33 @@ public class UIController : MonoBehaviour {
 	void Start () {
         playerShooting = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).GetComponent<Tir_normal>();
 		mapPanel.SetActive (false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("escape")) {
-            if (menuCanvas.alpha == 0)
-                openMenu();
-            else
-                closeMenu();
-		} else if (Input.GetKeyDown(KeyCode.Tab))
-		{
-			if (mapPanel.activeSelf == false)
-				mapPanel.SetActive(true);
+			if (menuCanvas.alpha == 0)
+				openMenu ();
 			else
-				mapPanel.SetActive(false);
+				closeMenu ();
+		} else if (Input.GetKeyDown (KeyCode.Tab)) {
+			if (mapPanel.activeSelf == false)
+				mapPanel.SetActive (true);
+			else
+				mapPanel.SetActive (false);
+		} else if (Input.GetKeyDown (KeyCode.M)) {
+			if (worldMapCanvas.alpha == 0) {		
+				worldMapCanvas.alpha = 1;
+				worldMapCanvas.interactable = true;
+				worldMapCanvas.blocksRaycasts = true;
+				(cam.GetComponent ("BlurOptimized") as MonoBehaviour).enabled = true;
+			} else {
+				worldMapCanvas.alpha = 0;
+				worldMapCanvas.interactable = false;
+				worldMapCanvas.blocksRaycasts = false;
+				(cam.GetComponent("BlurOptimized") as MonoBehaviour).enabled = false;
+			}
 		}
         ammoSlider.value = playerShooting.clip;
         ammoIndicator.text = playerShooting.clip.ToString();
