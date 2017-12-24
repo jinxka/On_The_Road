465aa22e88e37c5ce8638b794f798421f9bd8d54
Ram <ramy.chidiac@live.fr>
2017-12-24 20:06:36 +0100

    Improvements to new inventory

---
+++ Assets/Scripts/New Inventory/NewInventoryTooltip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewInventoryTooltip : MonoBehaviour {
+
+    [SerializeField]
+    Image itemIcon;
+
+    [SerializeField]
+    Text itemName;
+
+    [SerializeField]
+    Text itemDescription;
+
+    [SerializeField]
+    CanvasGroup tooltipCanvasGroup;
+
+    public void Start()
+    {
+        tooltipCanvasGroup.alpha = 0;
+    }
+
+    public void showTooltip(NewItem itemToShow)
+    {
+        itemIcon.sprite = itemToShow.itemIcon;
+        itemName.text = itemToShow.itemName;
+        itemDescription.text = itemToShow.itemDescription;
+        tooltipCanvasGroup.alpha = 1;
+    }
+
+    public void hideTooltip()
+    {
+        tooltipCanvasGroup.alpha = 0;
+    }
+}
---
+++ Assets/Scripts/New Inventory/NewItem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewItem : MonoBehaviour, IPointerDownHandler
+{
+
+    [Header("Item Display Properties")]
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemIcon;
+    
+    [Space(10)]
+    [Header("Item Characteristics")]
+    public itemTypes itemType;
+    public enum itemTypes { weapon, head, shoulders, legs, boots, gloves };
+
+    NewInventoryTooltip inventoryTooltip;
+    NewInventory inventory;
+
+    void Start()
+    {
+        inventoryTooltip = GameObject.FindGameObjectWithTag("NewInventoryTooltip").GetComponent<NewInventoryTooltip>();
+        inventory = GameObject.FindGameObjectWithTag("NewInventory").GetComponent<NewInventory>();
+    }
+
+    public void showItemInfo()
+    {
+        inventoryTooltip.showTooltip(this);
+    }
+
+    public void hideItemInfo()
+    {
+        inventoryTooltip.hideTooltip();
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (data.button == PointerEventData.InputButton.Right)
+        {
+            inventory.equipItem(this);
+        }
+    }
+}
