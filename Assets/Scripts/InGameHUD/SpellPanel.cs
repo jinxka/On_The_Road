9e3da2c322cd4dd9d6f9e3fbd577605ecb28343a
Ram <ramy.chidiac@live.fr>
2017-12-31 13:02:55 +0100

    Inventory Drag and drop

--- Assets/Scripts/InGameHUD/SpellPanel.cs
+++ Assets/Scripts/InGameHUD/SpellPanel.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class SpellPanel : MonoBehaviour {
 
---
+++ Assets/Scripts/New Inventory/NewItemAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NewItemAttribute {
+    public string attributeName;
+    public int attributeValue;
+
+    public NewItemAttribute(string attributeName, int attributeValue)
+    {
+        this.attributeName = attributeName;
+        this.attributeValue = attributeValue;
+    }
+
+    public NewItemAttribute() { }
+}
