14a633a850d137e0ffde312a25f2d3ad9a3ea337
Benjamin BALANGER <benjamin.balanger@wanadoo.fr>
2018-01-18 13:22:09 +0100

    Death Message

---
+++ Assets/Scripts/InGameHUD/DeathMessage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeathMessage : MonoBehaviour {
+
+    [SerializeField]
+    CanvasGroup progressGroup;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    // Use this for initialization
+    public void FixedUpdate()
+    {
+        if (player.GetComponent<PlayerHealth>().currentHealth <= 0)
+        {
+            progressGroup.alpha = 1;
+            if (Input.anyKey)
+            {
+                SceneLoading.Instance.loadScene(SceneManager.GetActiveScene().name);
+                Destroy(this);
+            }
+        }
+    }
+}
