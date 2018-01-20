32e667aea8b7eb6677a5eae030d55d94a5d79dfe
balang_b <benjamin.balanger@epitech.eu>
2018-01-20 19:58:00 +0100

    Update startMessage

--- Assets/Scripts/StartMenu/StartMessage.cs
+++ Assets/Scripts/StartMenu/StartMessage.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMessage : MonoBehaviour {
 
     [SerializeField]
     CanvasGroup progressGroup;
 
+    private float time;
+    private Text message;
+    private bool update = false;
     // Use this for initialization
+    void Start()
+    {
+        time = Time.time;
+        message = GetComponentInChildren<Text>();
+    }
+
     public void FixedUpdate()
     {
         progressGroup.alpha = 1;
-        if (Input.anyKey)
+        if (Time.time - time > 1)
         {
-            progressGroup.alpha = 0;
-            Destroy(this);
+            if (!update)
+            {
+                update = true;
+                message.text = message.text + "\n\n\n <b><i>PRESS ENTER TO CONTINUE</i></b>";
+            }
+            if (Input.GetKeyDown("return"))
+            {
+                progressGroup.alpha = 0;
+                Destroy(this);
+            }
         }
     }
 }
