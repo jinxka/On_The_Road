24534508d588ad927fb3107ab3e095b36159b374
balang_b <benjamin.balanger@epitech.eu>
2018-01-26 17:34:28 +0100

    Update Victory messages

--- Assets/Scripts/Gameplay/Script_Enemy_bullet.cs
+++ Assets/Scripts/Gameplay/Script_Enemy_bullet.cs
@@ -21,7 +21,6 @@ public class Script_Enemy_bullet : MonoBehaviour {
     {
         if (col.gameObject.tag == "Player")
         {
-            print("Player");
             PlayerHealth playerHealth = col.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
@@ -31,7 +30,6 @@ public class Script_Enemy_bullet : MonoBehaviour {
         }
         else if (col.gameObject.tag != "Enemies" && col.gameObject.tag == "Wall")
         {
-            print("OTHER");
             Destroy(gameObject);
         }
     }
